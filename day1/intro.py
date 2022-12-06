c03e6093d4832103f0cc698a18e4a4ca162af656
Sintoyia <lareenmary@gmail.com>
2022-12-07 00:17:34 +0300

    changed

--- day1/intro.py
+++ day1/intro.py
@@ -5,3 +5,20 @@ print ("division=", 3/4)
 print ("modulus=", 3%4)
 print ("exponential=", 3**4)
 print ("floor division=", 3//4)
+
+#number3
+print ("lareen")
+print("ubuntu")
+print("Kenya")
+
+#level 3
+print("integer=",-1)
+print("float=",-5.6)
+print("string=", "hello world")
+print("is the light on?", True)
+print 
+{
+   "name": "lareen",
+   "OS": "ubuntu",
+    "age": "435",
+}
