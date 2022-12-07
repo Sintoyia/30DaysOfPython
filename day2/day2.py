a0b93750f5ead369ceced001c9a924e7d22a5460
Sintoyia <lareenmary@gmail.com>
2022-12-07 20:13:51 +0300

    amended

--- day2/day2.py
+++ day2/day2.py
@@ -5,3 +5,23 @@ print ("ubuntu", "age", "country")
 # stored variables
 print ("skills:", "python")
 print ("quote:", "alchemist")
+
+ #python rules
+print ("alpha_")
+print ("year_2022")
+print ("birth_year_2")
+print ("personal_info=")
+print
+{
+   "name": "lareen",
+   "OS": "ubuntu",
+    "age": "435",
+    "city": "nairobi"
+}
+#declaring variables
+first_name = input("you guy: ")
+lastname = input("my guy: ")
+
+print(first_name)
+print(lastname)
+ 
