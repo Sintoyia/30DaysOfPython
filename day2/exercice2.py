ffe91ed3ef7804d396d0c51f0cc673f123f2fac0
Sintoyia <lareenmary@gmail.com>
2022-12-08 20:45:51 +0300

    examples

---
+++ day2/exercice2.py
@@ -0,0 +1,26 @@
+#declare variable
+firstname =input ("firstname")
+lastname =input ("lastname")
+first,last =input ("first"),input("last")
+country =input ("country")
+city =input("city")
+age =input("age")
+year =input("year")
+print("is sisi married?" , True)
+print("joy", "peace")
+print("lareen length:" , len("lareen"))
+
+num_one= 5
+num_two= 4
+print ("total=", num_one + num_two)
+print ("variable product=", num_one*num_two)
+print ("variable exp=", num_one**num_two)
+
+#circle
+radius=30
+print("area_of_circle=", 3.142 * radius**2)
+print("circum_of_circle=", 3.142*2*radius)
+
+radius= 20
+print("area=", 3.142*radius**2)
+
