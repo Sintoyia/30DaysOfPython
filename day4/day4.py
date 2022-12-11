d79bcdaa63ca06463a3b3149531502f064f35ad3
Sintoyia <lareenmary@gmail.com>
2022-12-11 18:35:57 +0300

    day4

---
+++ day4/day4.py
@@ -0,0 +1,76 @@
+
+print("everyday is fresh from God.\nWe are grateful")
+print("i will be great\tcoz i was meant to be")
+print("day\tTopics\tExamples")
+print("day1\t3\t8")
+print("backslash\\:")
+print("double quote\"")
+print("single quote\'")
+
+company="mazda"
+print(company[::-1])
+
+
+Religion ="Engineering"
+Caste ="Mechanical"
+
+formated_string= "I have studied %s. Specialized in %s" %(Religion, Caste)
+print (formated_string)
+
+firstname= "me"
+language= "python"
+formated_string= "I am %s. I love %s" %(firstname, language)
+print (formated_string)
+
+#New style
+a=6
+b=7
+c=8
+print("{} +{} -{} ={}". format (a, b, c, a+b-c))
+
+#exercise
+#concatenate
+first ="Thirty"
+space =" "
+middle ="Days"
+space =" "
+mid ="Of"
+space =" "
+last ="Python"
+concatenate = first+space+middle+space+mid+space+last
+print (concatenate)
+company= "Coding for all"
+print(company)
+cfa ="company"
+print (cfa)
+print(len("company"))
+company =company.upper()
+print(company)
+
+company =company.lower()
+print(company)
+
+company = "Coding For All"
+company = company.capitalize()
+company = company.title()
+company = company.swapcase()
+print(company)
+
+company= "Coding For all"
+first_six= company[0:6]
+print(first_six)
+last_three= company[10:14]
+print(last_three)
+
+
+
+
+
+
+
+
+
+
+
+
+
