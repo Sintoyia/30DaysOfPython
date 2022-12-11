4abda002661e554d4b25eb72398404ed3f3cca33
Sintoyia <lareenmary@gmail.com>
2022-12-11 16:02:40 +0300

    day3

---
+++ day3/day3examples.py
@@ -0,0 +1,34 @@
+#exercise3
+print ("age:", "30")
+print ("height:", "5.7")
+a = 6j
+print ("complex number:", "a")
+
+#enter base and height
+base= int ( input("enter base"))
+height= int( input("enter height"))
+
+area= base*height/2
+
+#enter sides
+sidea= input("enter sidea")
+sideb= input("enter sideb")
+sidec= input("enter sidec")
+perimeter= sidea+sideb+sidec
+
+#perimeter and area
+length= int(input("enter length"))
+width= int(input("enter width"))
+
+area= length * width
+perimeter= 2*(length+width)
+
+#circle
+radius= int(input("enter radius"))
+area=3.142*radius**2
+
+
+
+
+
+
