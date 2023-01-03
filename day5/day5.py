6fdd51b9bb9402a8868c96399beb95b597337c02
Sintoyia <lareenmary@gmail.com>
2023-01-03 12:54:16 +0300

    day5.py

---
+++ day5/day5.py
@@ -0,0 +1,21 @@
+empty_list= []
+print (empty_list)
+
+#list with initial values
+
+fruits = ['banana', 'orange', 'mango', 'lemon']     
+
+# Print the lists and its length
+print('Fruits:', fruits)
+
+fruits = ['banana', 'orange', 'mango', 'lemon']
+first_fruit = fruits[-4]
+
+print(first_fruit)      # banana
+
+list=["item1", "item2"]
+list= "first_item", "second_item"
+first_item= "banana"
+second_item= "orange"
+print(first_item)
+print(second_item)
